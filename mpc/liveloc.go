d3e51d712154ce0af0d8265961ff2c2f912cdb25
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-12 17:56:07 +0700

    print test

--- mpc/liveloc.go
+++ mpc/liveloc.go
@@ -1,6 +1,8 @@
 package mpc
 
 import (
+	"fmt"
+
 	"github.com/aiteung/atmessage"
 	"go.mau.fi/whatsmeow"
 	waProto "go.mau.fi/whatsmeow/binary/proto"
@@ -9,6 +11,8 @@ import (
 
 func LiveLocinPrivateMessage(Message *waProto.Message, Info *types.MessageInfo, im *atmessage.IteungMessage, waclient *whatsmeow.Client) {
 	msg := "Hai, " + Info.PushName + ". Nomor kamu " + Info.Sender.User + " belum terdaftar di layanan kami. Silakan gunakan nomor whatsapp yang terdaftar. Terima kasih"
+	fmt.Println(msg)
+	fmt.Println(Info.Sender)
 	atmessage.SendMessage(msg, Info.Sender, waclient)
 	print("hai")
 }
