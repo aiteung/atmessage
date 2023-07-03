54712da183b5fb99370677f056e4e3452f7949da
rofinafiin <rofinafiisr@gmail.com>
2023-07-03 07:49:59 +0100

    repair for grup id kelas pengganti

--- autoiteung/autoiteung.go
+++ autoiteung/autoiteung.go
@@ -11,8 +11,9 @@ func BukaKelas(group_name string) string {
 	_, err := strconv.Atoi(strings.TrimSpace(listgroup[0]))
 	if (len(listgroup) >= 3) && (err == nil) {
 		tokendosenpengganti := strings.Split(listgroup[2], "|")
-		if len(tokendosenpengganti) > 1 {
-			messages = "iteung kelas luring dosen pengganti mulai mode tm passcode " + strings.TrimSpace(tokendosenpengganti[1])
+		token := strings.Split(tokendosenpengganti[1], "@")
+		if len(token) > 1 {
+			messages = "iteung kelas luring dosen pengganti mulai mode tm passcode " + strings.TrimSpace(token[0])
 		} else {
 			messages = "iteung kelas luring mulai mode tm"
 		}
--- autoiteung/autoiteung_test.go
+++ autoiteung/autoiteung_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestBukaKelas(t *testing.T) {
-	nama_group := "21666-2A-PEMOGRAMAN III | TYGUSAD"
+	nama_group := "21666-2A-PEMOGRAMAN III | TYGUSAD@62114545454"
 	pesaniteung := BukaKelas(nama_group)
 	fmt.Println(pesaniteung)
 
