276c42ed975b18d8017124d2d29a640d9180f5ec
JPratama7 <josepratama080@gmail.com>
2023-07-05 16:40:36 +0700

    len dulu

--- autoiteung/autoiteung.go
+++ autoiteung/autoiteung.go
@@ -11,8 +11,8 @@ func BukaKelas(group_name string) string {
 	_, err := strconv.Atoi(strings.TrimSpace(listgroup[0]))
 	if (len(listgroup) >= 3) && (err == nil) {
 		tokendosenpengganti := strings.Split(listgroup[2], "|")
-		token := strings.Split(tokendosenpengganti[1], "@")
-		if len(token) > 1 {
+		if len(tokendosenpengganti) > 1 {
+			token := strings.Split(tokendosenpengganti[1], "@")
 			messages = "iteung kelas luring dosen pengganti mulai mode tm passcode " + strings.TrimSpace(token[0])
 		} else {
 			messages = "iteung kelas luring mulai mode tm"
