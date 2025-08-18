374a12c3f51755555bd741584ad26db9c1c1d74b
raffzhm <rafiazhim.ra@gmail.com>
2025-08-19 02:39:49 +0700

    Update: import

--- whatsapp.go
+++ whatsapp.go
@@ -8,7 +8,7 @@ import (
 	"github.com/aiteung/musik"
 	_ "github.com/mattn/go-sqlite3"
 	"go.mau.fi/whatsmeow"
-	waProto "go.mau.fi/whatsmeow/binary/proto"
+	waProto "go.mau.fi/whatsmeow/proto/waE2E"
 	"go.mau.fi/whatsmeow/store/sqlstore"
 	"go.mau.fi/whatsmeow/types"
 	waLog "go.mau.fi/whatsmeow/util/log"
